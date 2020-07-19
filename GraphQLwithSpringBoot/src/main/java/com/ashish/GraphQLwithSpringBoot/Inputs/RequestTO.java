b4240210e9bf9fb8ee70a5b083d2c2b0c2d0900d
unknown <ashishj.mallick@gmail.com>
2020-07-19 11:22:41 +0530

    graphQL implementation

---
+++ GraphQLwithSpringBoot/src/main/java/com/ashish/GraphQLwithSpringBoot/Inputs/RequestTO.java
@@ -0,0 +1,11 @@
+/*
+ *  @Author: Ashish Mallick
+ *  linkedIn : https://www.linkedin.com/in/ashish-mallick
+ */
+
+
+package com.ashish.GraphQLwithSpringBoot.Inputs;
+
+public class RequestTO {
+
+}
---
+++ GraphQLwithSpringBoot/src/test/java/com/ashish/GraphQLwithSpringBoot/GraphQLwithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashish.GraphQLwithSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQLwithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
